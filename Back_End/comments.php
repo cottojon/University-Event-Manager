b81e47f8edad4f90b1d12ecbc14f751d2a128d78
stevensKimberly <k.stevens7596@gmail.com>
2018-11-23 10:48:30 -0500

    Update comments.php
    Comments can be entered into the database

--- Back_End/comments.php
+++ Back_End/comments.php
@@ -2,12 +2,19 @@
 	require_once 'session.php';
 	require('connect.php');
 	
-	// Get comment and decode the decode the json
-	$comment = $_POST['insertComment'];
+	// Get comment and decode the json
+	$event = $_POST['eventID'];
+	$comment = $_POST['comment'];
+	$rating = $_POST['rate'];
 	
 	// put into table
-	$sql = "INSERT INTO comments(comment)
-			VALUES($comment)";
+	$sql = "INSERT INTO commentsAndRating(userID, commentText, eventID, rating)
+			VALUES(?, ?, ?, ?)";
 			
-	$insert = mysqli_query($connect, $sql);
+	$query = mysqli_prepare($connect, $sql);
+	$query->bind_param('isii', $_SESSION['logged-in-user'], $comment, $event, $rating);
+	$query->execute();
+	
+	$query->free_result();
+	$query->close();
 ?>
