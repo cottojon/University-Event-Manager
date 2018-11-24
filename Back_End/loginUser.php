733f99da73268ae127381c8e0561c511fdbecae1
stevensKimberly <k.stevens7596@gmail.com>
2018-11-24 12:53:53 -0500

    Update loginUser.php
    Cleaned up

--- Back_End/loginUser.php
+++ Back_End/loginUser.php
@@ -44,36 +44,4 @@
 	if (!$matchedUser) {
 		json_encode("No user found");
 	}
-	
-	//header('Location: myOtherPage.html'); // Redirection
-	//$_SESSION['logged-in-user'] = $userId; // Add data to session.
-	//unset($_SESSION['logged-in-user']); // Remove data from session.
-	
-	//$currentLoggedInUser = $_SESSION['logged-in-user'];
-	/*
-	// Examples of safe places to use regular sql without prepped stmt
-	$myUserInput = (int) $_POST['myParameter'];
-	$myConstant = 'happy birthday';
-	$sql = "select * from blah where columnValue = '$myConstant'";
-	exit(); // just to stop more execution in this specific script, don't actually do this.
-	// TODO !!! PREPARED STATEMENT
-	*/
-	/*
-	// Check that a row for this username and password exists.
-	$sqlChkUser = "SELECT userId
-				   FROM users
-				   WHERE userName = '$user' AND password = '$pass'";
-				   echo $sqlChkUser;
-	$result = mysqli_query($connect, $sqlChkUser);
-	$numResult = mysqli_num_rows($result);
-	*/
-	// Return error message if the information is incorrect.
-	//if($numResult != 1)
-	//{
-	//	exit("Invalid login information");
-	//}
-	
-	// $id holds the user id of the user to load the proper events later, other pages will require this information.
-	//$row = $result->fetch_assoc();
-	//$id = (int) $row['userId'];
 ?>
